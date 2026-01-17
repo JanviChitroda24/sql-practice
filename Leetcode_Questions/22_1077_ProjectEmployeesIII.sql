0e1731a3f7dc7eae7bb0ad8ac4d6b385b09f4fcc
JanviChitroda24 <janvichitroda0824@gmail.com>
2026-01-17 17:36:28 -0500

    Solve LC-1077: Project Employees III

---
+++ Leetcode_Questions/22_1077_ProjectEmployeesIII.sql
@@ -0,0 +1,73 @@
+"""
+1077 - Project Employees III
+https://leetcode.ca/all/1077.html
+
+Write an SQL query that reports the most experienced employees in each project. 
+In case of a tie, report all employees with the maximum number of experience years.
+
+Table: Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key of this table.
+employee_id is a foreign key to Employee table.
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key of this table.
+
+The query result format is in the following example:
+
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 3                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+
+Result table:
++-------------+---------------+
+| project_id  | employee_id   |
++-------------+---------------+
+| 1           | 1             |
+| 1           | 3             |
+| 2           | 1             |
++-------------+---------------+
+Both employees with id 1 and 3 have the most experience among the employees of the first project. For the second project, the employee with id 1 has the most experience.
+"""
+
+
+WITH emp_exp AS(
+	SELECT e.employee_id as employee_id, 
+        p.project_id as project_id,
+        DENSE_RANK() OVER(PARTITION BY p.project_id ORDER BY e.experience_years DESC) AS exp_rank
+  	FROM Employee e JOIN Project p	
+	ON e.employee_id = p.employee_id
+)
+SELECT project_id, employee_id
+FROM emp_exp
+WHERE exp_rank=1;
