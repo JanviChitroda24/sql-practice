3c2ee2b853977d7729aa2e878ce6f8def5079041
JanviChitroda24 <janvichitroda0824@gmail.com>
2026-05-17 06:56:15 -0400

    Add solution for 1355. Activity Participants

---
+++ Leetcode_Questions/77_1355_Activity Participants.sql
@@ -0,0 +1,77 @@
+-- 1355. Activity Participants
+-- https://leetcode.ca/all/1355.html
+
+-- 1355. Activity Participants
+-- SQL Schema 
+-- Table: Friends
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | name          | varchar |
+-- | activity      | varchar |
+-- +---------------+---------+
+-- id is the id of the friend and primary key for this table.
+-- name is the name of the friend.
+-- activity is the name of the activity which the friend takes part in.
+-- Table: Activities
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- id is the primary key for this table.
+-- name is the name of the activity.
+ 
+
+-- Write an SQL query to find the names of all the activities with neither maximum, nor minimum number of participants.Programming
+
+-- Return the result table in any order. Each activity in table Activities is performed by any person in the table Friends.
+
+-- The query result format is in the following example:
+
+-- Friends table:
+-- +------+--------------+---------------+
+-- | id   | name         | activity      |
+-- +------+--------------+---------------+
+-- | 1    | Jonathan D.  | Eating        |
+-- | 2    | Jade W.      | Singing       |
+-- | 3    | Victor J.    | Singing       |
+-- | 4    | Elvis Q.     | Eating        |
+-- | 5    | Daniel A.    | Eating        |
+-- | 6    | Bob B.       | Horse Riding  |
+-- +------+--------------+---------------+
+
+-- Activities table:
+-- +------+--------------+
+-- | id   | name         |
+-- +------+--------------+
+-- | 1    | Eating       |
+-- | 2    | Singing      |
+-- | 3    | Horse Riding |
+-- +------+--------------+
+
+-- Result table:
+-- +--------------+
+-- | results      |
+-- +--------------+
+-- | Singing      |
+-- +--------------+
+
+-- Eating activity is performed by 3 friends, maximum number of participants, (Jonathan D. , Elvis Q. and Daniel A.)
+-- Horse Riding activity is performed by 1 friend, minimum number of participants, (Bob B.)
+-- Singing is performed by 2 friends (Victor J. and Jade W.)
+
+WITH max_min_activity AS (
+    SELECT a.name AS results, COUNT(f.id) AS activity_count
+    FROM Activities a LEFT JOIN Friends f 
+        ON a.name = f.activity
+    GROUP BY a.name
+)
+SELECT results
+FROM max_min_activity
+WHERE activity_count != (SELECT MAX(activity_count) FROM max_min_activity) AND 
+    activity_count != (SELECT MIN(activity_count) FROM max_min_activity);
