faf50018d4ce87af5409825437c9f3e52f77a58d
JanviChitroda24 <janvichitroda0824@gmail.com>
2026-06-25 17:33:45 -0400

    Add practice solution for 1070. Product Sales Analysis III

---
+++ PracticeAgain/44_1070_ProductSalesAnalysisIII.sql
@@ -0,0 +1,67 @@
+-- 1070 — Product Sales Analysis III
+-- https://leetcode.com/problems/product-sales-analysis-iii/
+
+-- Table: Sales
+
+-- +-------------+-------+
+-- | Column Name | Type  |
+-- +-------------+-------+
+-- | sale_id     | int   |
+-- | product_id  | int   |
+-- | year        | int   |
+-- | quantity    | int   |
+-- | price       | int   |
+-- +-------------+-------+
+-- (sale_id, year) is the primary key (combination of columns with unique values) of this table.
+-- Each row records a sale of a product in a given year.
+-- A product may have multiple sales entries in the same year.
+-- Note that the per-unit price.
+
+-- Write a solution to find all sales that occurred in the first year each product was sold.
+-- For each product_id, identify the earliest year it appears in the Sales table.
+-- Return all sales entries for that product in that year.
+
+-- Return a table with the following columns: product_id, first_year, quantity, and price.
+-- Return the result in any order.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Sales table:
+-- +---------+------------+------+----------+-------+
+-- | sale_id | product_id | year | quantity | price |
+-- +---------+------------+------+----------+-------+ 
+-- | 1       | 100        | 2008 | 10       | 5000  |
+-- | 2       | 100        | 2009 | 12       | 5000  |
+-- | 7       | 200        | 2011 | 15       | 9000  |
+-- +---------+------------+------+----------+-------+
+
+-- Output: 
+-- +------------+------------+----------+-------+
+-- | product_id | first_year | quantity | price |
+-- +------------+------------+----------+-------+ 
+-- | 100        | 2008       | 10       | 5000  |
+-- | 200        | 2011       | 15       | 9000  |
+-- +------------+------------+----------+-------+
+
+
+-- 
+WITH sales_min_prod_year AS (
+    SELECT sale_id, product_id, year, quantity, price,
+        MIN(year) OVER(PARTITION BY product_id) AS min_year
+    FROM Sales
+)
+SELECT product_id, year AS first_year, quantity, price
+FROM sales_min_prod_year
+WHERE year = min_year;
+
+-- alternative solution with Join 
+SELECT s.product_id, s.year AS first_year, s.quantity, s.price
+FROM Sales s JOIN (
+        SELECT product_id, MIN(year) AS min_year
+        FROM Sales
+        GROUP BY product_id
+    ) sales_min sm 
+    ON s.product_id = sm.product_id AND s.year = sm.min_year
